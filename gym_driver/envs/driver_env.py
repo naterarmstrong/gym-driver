342b0eb6f36d1769e5f58d36fae78ce2a3a8bddc
Nate Armstrong <narmstrong@berkeley.edu>
2017-07-21 22:56:48 -0700

    small changes

--- gym_driver/envs/driver_env.py
+++ gym_driver/envs/driver_env.py
@@ -7,12 +7,14 @@ class DriverEnv(gym.Env):
 
   def __init__(self):
   	pass
+  	self.env = gateway.entry_point # Gets an instance of the environment
+  	# Something or other initializing the java code then creating the gateway, and setting gateway to it
 
   def _step(self, action):
-  	pass
+  	return self.env.step(action) # Assuming that this returns the observation tuple
 
   def _reset(self):
-  	pass
+  	self.env.reset()
 
   def _render(self, mode='human', close=False):
-  	pass
+  	self.env.render(mode, close)
