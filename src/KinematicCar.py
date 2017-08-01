1cc1e4df66736c585adc019148395d7d10937fe4
Library Laptop Lending <UCBerkeley@calvisitor-10-105-139-157.calvisitor.1918.berkeley.edu>
2017-07-31 21:10:19 -0700

    remove old dirs

--- src/KinematicCar.py
+++
@@ -1,9 +0,0 @@
-from Car import Car
-
-# KinematicCar class
-class KinematicCar(Car):
-
-    # Modify attributes to reflect one game step
-    def step(self, action_acc, action_angle):
-        # See Wesley's code
-        None
--- src/PointCar.py
+++
@@ -1,9 +0,0 @@
-from Car import Car
-
-# PointCar class
-class PointCar(Car):
-
-    # Modify attributes to reflect one game step
-    def step(self, action_acc, action_angle):
-        # See Wesley's code
-        None
