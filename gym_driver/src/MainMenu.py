9efac6641721d7d9b9aadd953fbaf9f100f3927f
robertSweeney <robertsw97@gmail.com>
2017-08-02 03:38:36 -0700

    First implementation of MainMenu

--- gym_driver/src/MainMenu.py
+++ gym_driver/src/MainMenu.py
@@ -1,58 +1,86 @@
-from MapMenu import MapMenu
-from MainPanel import MainPanel
-from Tkinter import *
-
-
+from tkinter import *
 
 frame_height = 600
-frame_width = 1200
+frame_width = 1000
+LETTER_SIZE = 10.25
 
 
-class MainMenu(MapMenu):
+class MainMenu(PanedWindow):
+    button_width = 20
+    button_height = 3 * LETTER_SIZE
+
     def __init__(self, root):
-        panel = MainPanel(root)
-        return;
+        PanedWindow.__init__(self)
+        self.root = root
+        self.displayMapName()
+        self.addButtons()
+        self.addMapPane()
+
+    def addButtons(self):
+        self.setEditButton()
+        self.setRunButton()
+        self.setNextButton()
+        self.setPrevButton()
+        self.setNewButton()
+        self.setSelectButton()
+
+    def displayMapName(self):
+        T = Text(root, height=2, width=30)
+        T.insert(END, "Map Name")
+        T.place(x = frame_width - self.button_width * LETTER_SIZE, y=0)
+
+    def start_edit_menu(self):
+        print("EDIT")
+
+    def setEditButton(self):
+        editButton = Button(self.root, text="Edit Map", command = self.start_edit_menu, width=self.button_width)
+        editButton.place(x = frame_width - self.button_width * LETTER_SIZE, y= 2 * LETTER_SIZE)
+
+    def setRunButton(self):
+        runButton = Button(self.root, text="Run Map", command=None, width=self.button_width)
+        runButton.place(x = frame_width - self.button_width * LETTER_SIZE, y = self.button_height + 2 * LETTER_SIZE)
+
+    def setNextButton(self):
+        nextButton = Button(self.root, text=">", command=None, width = self.button_width / 2)
+        nextButton.place(x = frame_width - self.button_width * LETTER_SIZE / 2, y = 2 * self.button_height + 2 * LETTER_SIZE)
+
+    def setPrevButton(self):
+        prevButton = Button(self.root, text="<", command=None, width = self.button_width / 2)
+        prevButton.place(x = frame_width - self.button_width * LETTER_SIZE, y = 2 * self.button_height + 2 * LETTER_SIZE)
+
+    def setSelectButton(self):
+        selectButton = Button(self.root, text="Select Map", command=None, width = self.button_width)
+        selectButton.place(x=frame_width - self.button_width * LETTER_SIZE, y= 3 * self.button_height + 2 * LETTER_SIZE)
+
+    def setNewButton(self):
+        newButton = Button(self.root, text="NEW MAP", command=None, width = self.button_width)
+        newButton.place(x = frame_width - self.button_width * LETTER_SIZE, y = frame_height - self.button_height)
+
+    def addMapPane(self):
+        master = PanedWindow(bg="blue")
+        master.pack(side=LEFT)
+        master.propagate(0)
+        canvas = Canvas(master, scrollregion=(0, 0, frame_width, frame_height), bg="green")
+        hbar = Scrollbar(master, orient=HORIZONTAL)
+        hbar.pack(side=BOTTOM, fill=X)
+        hbar.config(command=canvas.xview)
+        vbar = Scrollbar(master, orient=VERTICAL)
+        vbar.pack(side=RIGHT, fill=Y)
+        vbar.config(command=canvas.yview)
+        canvas.config(width=300, height=300)
+        canvas.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set)
+        canvas.pack(side=LEFT, expand=True, fill=BOTH)
+
+        master.config(width=frame_width - self.button_width * LETTER_SIZE, height=frame_height)
+        master.add(canvas)
+
+
 
 root = Tk()
-frame = Frame(root, height = frame_height, width = frame_width)
-frame.pack()
-MainMenu(root)
+root.wm_title("Menu")
+root.resizable(width=False, height=False)
+root.config(width = frame_width, height = frame_height)
+root.propagate(0)
+frame = Frame(root)
+main_menu = MainMenu(root)
 root.mainloop()
-
-# import javax.swing.JFrame;
-# import javax.swing.WindowConstants;
-#
-# import java.awt.Dimension;
-#
-# /** MainMenu class */
-# class MainMenu extends MapMenu {
-#
-#     /** MainMenu constructor */
-#     MainMenu() {
-#         super(new Map(0, 0));
-#     }
-#
-#     /** Make a new Panel for the MainMenu */
-#     MainPanel makePanel() {
-#         return new MainPanel(this);
-#     }
-#
-#     /** Get the MainMenu's title */
-#     String getTitle() {
-#         return "Main Menu";
-#     }
-#
-#     /** Run the FORDS app */
-#     public static void main(String[] args) {
-#         // Initialize the window
-#         JFrame frame = new JFrame();
-#         frame.setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
-#         frame.setLocationByPlatform(true);
-#         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-#         frame.setVisible(true);
-#         frame.setResizable(false);
-#         // Initialize the MainMenu
-#         Panel.newScreen(frame, new MainMenu());
-#     }
-#
-# }
